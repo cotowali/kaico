17536dd3b709ca016c7413facf83bd6763bbae5c
zakuro <z@kuro.red>
2024-04-06 07:48:36 +0900

    Token: extend token kind and constructors

--- kaico/token/token.v
+++ kaico/token/token.v
@@ -7,14 +7,88 @@ module token
 import kaico.source { Range }
 import kaico.utils { unwrap_none }
 
-pub enum TokenKind {
-	int_literal
+// ---
+// int
+// ---
+
+pub enum IntBase {
+	binary  = 2
+	octal   = 8
+	decimal = 10
+	hex     = 16
 }
 
+pub struct Int {
+pub:
+	base  IntBase
+	value i64
+}
+
+pub fn Token.new_int(base IntBase, value i64, range Range) Token {
+	return Token.new_literal(Int{
+		base: base
+		value: value
+	}, range)
+}
+
+// ----
+// bool
+// ----
+
+pub enum Bool {
+	true_
+	false_
+}
+
+pub fn Token.new_bool(kind Bool, range Range) Token {
+	return Token.new_literal(kind, range)
+}
+
+pub type Literal = Bool | Int
+
+pub enum Operator {
+	add
+	sub
+	mul
+	div
+}
+
+// -------
+// unknown
+// -------
+
+pub struct Unknown {}
+
+pub fn Token.new_literal(literal Literal, range Range) Token {
+	return Token.new(literal, range)
+}
+
+pub fn Token.new_operator(op Operator, range Range) Token {
+	return Token.new(op, range)
+}
+
+pub fn Token.new_unknown(range Range) Token {
+	return Token.new(Unknown{}, range)
+}
+
+// -----
+// token
+// -----
+
+pub type TokenKind = Literal | Operator | Unknown
+
+@[noinit]
 pub struct Token {
 pub:
 	kind  TokenKind
 	range Range
+}
+
+pub fn Token.new(kind TokenKind, range Range) Token {
+	return Token{
+		kind: kind
+		range: range
+	}
 }
 
 pub fn (t Token) text() string {
--- kaico/token/token_test.v
+++ kaico/token/token_test.v
@@ -2,15 +2,50 @@
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-import kaico.token as _ { Token }
+import kaico.token as _ {
+	Bool,
+	Int,
+	Literal,
+	Operator,
+	Token,
+	TokenKind,
+	Unknown,
+}
 import kaico.source as _ { Range, Source }
 
 fn test_text() {
-	s := Source.from_text('01234')
+	s := Source.from_text('0 23 4')
 	r := Range.new(s.new_pos(offset: 2, line: 1, column: 3), s.new_pos(offset: 4, line: 1, column: 5))
-	t := Token{
-		kind: .int_literal
-		range: r
-	}
+	t := Token.new_int(.decimal, 23, r)
 	assert t.text() == '23'
+}
+
+fn test_new() {
+	range_for := fn (text string) Range {
+		s := Source.from_text(text)
+		n := text.len
+		p1 := s.new_pos(offset: 0, line: 1, column: 1)
+		p2 := s.new_pos(offset: n, line: 1, column: n + 1)
+		return Range.new(p1, p2)
+	}
+
+	lit := Token.new_literal(Bool.true_, range_for('true'))
+	assert lit.kind == TokenKind(Literal(Bool.true_))
+
+	int_d := Token.new_int(.decimal, 1, range_for('0b1'))
+	assert int_d.kind is Literal
+	assert (int_d.kind as Literal) is Int
+	assert ((int_d.kind as Literal) as Int).base == .decimal
+
+	bool_true := Token.new_bool(.true_, range_for('true'))
+	assert bool_true.kind is Literal
+	assert (bool_true.kind as Literal) is Bool
+	assert (bool_true.kind as Literal) as Bool == .true_
+
+	op := Token.new_operator(.add, range_for('+'))
+	assert op.kind is Operator
+	assert op.kind as Operator == .add
+
+	un := Token.new_unknown(range_for(' '))
+	assert un.kind is Unknown
 }
