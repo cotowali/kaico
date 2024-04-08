7da67f5b7fc932347b7f5bed6a4b4b86f882b112
zakuro <z@kuro.red>
2024-04-08 18:32:35 +0900

    Tweak license text

--- kaico/builder/builder.v
+++ kaico/builder/builder.v
@@ -1,7 +1,10 @@
-// Copyright (c) 2021-2023 The Kaico Authors.
+// SPDX-License-Identifier: MPL-2.0
+//
+// Copyright 2021-2024 zakuro <z@kuro.red> (https://x.com/zakuro9715)
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 module builder
 
 import kaico.source as _ { Source }
